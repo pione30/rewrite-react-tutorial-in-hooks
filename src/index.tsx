fd0b7ca928e1c9c228b71d758bfd82954d51d2d6
pione30 <g.pionelish30@gmail.com>
2019-08-03 01:27:36 +0900

    treat value as state

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,22 @@ type SquareProps = {
   value: number
 }
 
-class Square extends React.Component<SquareProps> {
+type SquareState = {
+  value: string | null
+}
+
+class Square extends React.Component<SquareProps, SquareState> {
+  constructor(props: SquareProps) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
+
   render() {
     return (
-      <button className="square" onClick={ () => alert('click') }>
-        {this.props.value}
+      <button className="square" onClick={ () => this.setState({value: 'X'}) }>
+        {this.state.value}
       </button>
     );
   }
