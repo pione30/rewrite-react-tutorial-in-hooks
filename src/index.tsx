c3f7dc0d38d5a4442a38e5aa8279479351634077
pione30 <g.pionelish30@gmail.com>
2019-08-03 01:18:34 +0900

    onClick={ () => alert('click') }

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ type SquareProps = {
 class Square extends React.Component<SquareProps> {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={ () => alert('click') }>
         {this.props.value}
       </button>
     );
