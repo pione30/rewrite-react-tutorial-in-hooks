3b7a9ba7474ddc251d4e8630b49741b7b8b2c6cd
pione30 <g.pionelish30@gmail.com>
2019-08-04 17:23:52 +0900

    create and display the list of history

--- src/index.tsx
+++ src/index.tsx
@@ -114,6 +114,18 @@ class Game extends React.Component<any, GameState> {
     const current = history[history.length - 1]
     const winner = calculateWinner(current.squares)
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start'
+
+      return (
+        <li>
+          <button onClick={ () => this.jumpTo(move) }>{ desc }</button>
+        </li>
+      )
+    })
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner
@@ -132,7 +144,7 @@ class Game extends React.Component<any, GameState> {
         </div>
         <div className="game-info">
           <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
