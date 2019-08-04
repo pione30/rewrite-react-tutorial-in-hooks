479e81c1aa1e3a2fa0c5c0f8194ded87cec764b1
pione30 <g.pionelish30@gmail.com>
2019-08-04 18:00:14 +0900

    move calculateWinner to the bottom

--- src/index.tsx
+++ src/index.tsx
@@ -53,26 +53,6 @@ class Board extends React.Component<BoardProps> {
   }
 }
 
-function calculateWinner(squares: Array<string | null>) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 type GameState = {
   history: Array<{
     squares: Array<string | null>
@@ -159,6 +139,26 @@ class Game extends React.Component<any, GameState> {
       </div>
     );
   }
+}
+
+function calculateWinner(squares: Array<string | null>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
