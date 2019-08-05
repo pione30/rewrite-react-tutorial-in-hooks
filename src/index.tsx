e101b77725a1e399bbb5a229cbf00056f13cf413
pione30 <g.pionelish30@gmail.com>
2019-08-06 00:50:07 +0900

    remove `this` because Game is no longer Class

--- src/index.tsx
+++ src/index.tsx
@@ -92,46 +92,43 @@ const Game = () => {
     })
   }
 
-  render() {
-    const history = this.state.history
-    const current = history[this.state.stepNumber]
-    const winner = calculateWinner(current.squares)
+  const current = history[stepNumber]
+  const winner = calculateWinner(current.squares)
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start'
-
-      return (
-        <li key={ move }>
-          <button onClick={ () => this.jumpTo(move) }>{ desc }</button>
-        </li>
-      )
-    })
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-    }
+  const moves = history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start'
 
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={ current.squares }
-            onClick={ (i: number) => this.handleClick(i) }
-          />
+      <li key={ move }>
+        <button onClick={ () => jumpTo(move) }>{ desc }</button>
+      </li>
+    )
+  })
 
-        </div>
-        <div className="game-info">
-          <div>{ status }</div>
-          <ol>{ moves }</ol>
-        </div>
-      </div>
-    );
+  let status: string;
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O')
   }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={ current.squares }
+          onClick={ (i: number) => handleClick(i) }
+        />
+
+      </div>
+      <div className="game-info">
+        <div>{ status }</div>
+        <ol>{ moves }</ol>
+      </div>
+    </div>
+  )
 }
 
 function calculateWinner(squares: Array<string | null>) {
